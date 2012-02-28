fe9e260aa927cfaf3609c9db1046ab984adb1cb5
Bradley Momberger <air_hadoken@yahoo.com>
2012-02-27 23:44:51 -0500

    Somewhat optimized the particle display routine, added spotlight sweep effect, swapped black/white

--- spotlight-text.js
+++ spotlight-text.js
@@ -1,43 +1,81 @@
+/**
+   spotlight-text/spotlight-text.js
+   (C) 2012 Bradley Momberger.
+   @version 1.0
+   Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
+
+   Notes:
+   This library will draw into a canvas the text specified in the text box, with a "sweeping spotlight" animation that both shows and hides the text.
+*/
 (function(){
      var interval = 2000, //(ms)
      context = $("#layer_0")[0].getContext("2d"),
      maxradius = Math.sqrt((120 + 100) * (120 + 100) + (600-230) * (600-230)),
-     width = 600,
-     height = 120,
+     width = $("#layer_0").width(),
+     height = $("#layer_0").height(),
      radialOffsetX = 230,
-     radialOffsetY = 220;
+     radialOffsetY = 220,
+     textOffsetX = 50,
+     textOffsetY = 85;
 
     function drawFrame() {
-      context.clearRect(0, 0, width, height);
+	context.fillStyle= "rgb(0,0,0)"
+      context.fillRect(0, 0, width, height);
+      context.fillStyle = "rgb(255, 255, 255)";
 	context.save();
 	context.beginPath();
 	context.moveTo(radialOffsetX, radialOffsetY);
 	//Create fraction of interval time as fraction of circle in radians
         var t = new Date().getTime() % interval / interval * Math.PI * 2;
 	context.arc(radialOffsetX, radialOffsetY, maxradius, t, t + Math.PI, false );
-	var vectorX = -maxradius * Math.cos(t);
-	var vectorY = maxradius * Math.sin(t);
-	drawParticles(vectorX, vectorY);
+	var vectorX = -maxradius * Math.sin(t);
+	var vectorY = maxradius * Math.cos(t);
+	drawParticles(vectorX, vectorY, 1);
 
 	context.closePath();
 	context.clip();
 
       context.font = "75px sans-serif";
-      context.fillStyle = "rgb(0, 0, 0)";
-      context.fillText($("#text_entry").val(), 50, 100);
+      context.fillText($("#text_entry").val(), textOffsetX, textOffsetY);
 	context.restore(); // reset clipping window
+
+	context.beginPath();
+	context.moveTo(radialOffsetX, radialOffsetY);
+	context.arc(radialOffsetX, radialOffsetY, maxradius, t + Math.PI - 0.5, t + Math.PI, false );
+	context.fill();
+
+	context.beginPath();
+	context.moveTo(radialOffsetX, radialOffsetY);
+	context.arc(radialOffsetX, radialOffsetY, maxradius, t - 0.5, t, false );
+	context.fill();
+
+	vectorX = -maxradius * Math.sin(t - 0.5);
+	vectorY = maxradius * Math.cos(t - 0.5);
+	drawParticles(vectorX, vectorY, -1);
+
       setTimeout(drawFrame, 10);
     }
 
-     function drawParticles(vectorX, vectorY) {
-	 for(var i = 0; i < 2000 ; i++) {
-	     var offsetX = Math.random();
-	     offsetX = Math.pow(offsetX, 4) * maxradius;
-	     var signX = Math.random() >= 0.5 ? 1 : -1;
+     function drawParticles(vectorX, vectorY, direction) {
+	 if(vectorX == 0)return;
+	 for(var i = 0; i < height ; i+=2) {
+	     var baseX = vectorY / vectorX * (i + radialOffsetY - height) + radialOffsetX;
 	     var t = Math.random();
-	     var offsetY = radialOffsetY - t * vectorY;
-	     offsetX = t * (vectorX) + signX * t * offsetX + radialOffsetX;
-	     context.fillRect(offsetX, offsetY, 1, 1);
+	     var offsetY = height - i;
+	     var offsetX = direction * Math.random() * (width - baseX);
+	     var x = Math.random(), y = 1-x, z = x-y/2;
+	     offsetX *= z;
+	     context.fillRect(baseX + offsetX, offsetY, 2, 2);
+	     offsetX *= x;
+	     context.fillRect(baseX + offsetX, offsetY, 2, 2);
+	     offsetX *= y;
+	     context.fillRect(baseX + offsetX, offsetY, 2, 2);
+	     offsetX *= z;
+	     context.fillRect(baseX + offsetX, offsetY, 2, 2);
+	     offsetX *= x;
+	     context.fillRect(baseX + offsetX, offsetY, 2, 2);
+	     offsetX *= y;
+	     context.fillRect(baseX + offsetX, offsetY, 2, 2);
 	 }
      }
 
